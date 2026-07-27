e55811f487d769b0c3ed80bae7c98ffcb65569d6
insightlabs38-pixel <insightlabs38@gmail.com>
2026-07-27 18:12:14 -0400

    Load the presentation-mode module

--- sim.js
+++ sim.js
@@ -1,3 +1,4 @@
 Promise.resolve()
   .then(() => import('./sim.mjs'))
-  .then(() => import('./enhancements.mjs'));
+  .then(() => import('./enhancements.mjs'))
+  .then(() => import('./presentation-boost.mjs'));
